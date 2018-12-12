303e3d7aa9af7da861b9c704f740bb86e9fa1986
Nitin <er.nitinpatel914@gmail.com>
2018-12-11 22:00:38 -0600

    added information of the author

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os 
 
-from flask import Flask, redirect, render_template #, Blueprint
+from flask import Flask, redirect, render_template, jsonify#, Blueprint
 # from flask_restful import Api
 from flask_restplus import Api
 from flask_jwt import JWT
@@ -11,11 +11,19 @@ from resources.topic import Topic, Class, Title, PageNotFound
 from db import db
 
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'Nitinkumar'
+
+app.__author__ = "Nitinkumar Patel"
+app.__copyright__ = "Copyright 2018, The Nitin_must Project"
+app.__credits__ = ["Nitin Patel"]
+app.__license__ = "Must"
+app.__version__ = "1.0.1"
+app.__maintainer__ = "Nitin Patel"
+app.__email__ = "er.nitinpatel914@gmail.com"
+app.__status__ = "Production"
 
 #@ Nitinkumar
 #In the README.txt, this project includes the artifacts for the 2 tasks: 
@@ -23,6 +31,7 @@ app.secret_key = 'Nitinkumar'
 # api = Api(blueprint) #,doc=False
 api = Api(app) #,doc=False
 PREFIX = "/eb"
+ENV = "QA"
 
 # app.register_blueprint(blueprint)
 
@@ -37,6 +46,9 @@ PREFIX = "/eb"
 def topics():
     return redirect(PREFIX+'/class/topic', code=302)
 
+@app.route(PREFIX+'/contact_author')
+def author():
+    return jsonify({"Author":app.__author__, "E-Mail":app.__email__}), 200
 # @app.errorhandler(404)
 # def page_not_found(e):
 #     # note that we set the 404 status explicitly
@@ -44,11 +56,13 @@ def topics():
 
 api.add_resource(Class, PREFIX+'/class/<string:name>')
 api.add_resource(Topic, PREFIX+'/topic/<int:_id>')
-api.add_resource(Title, PREFIX+'/title/<string:title>')
+api.add_resource(Title, PREFIX+'/title/<string:title>') # extra api /eb/title/<urltitle>
 api.add_resource(PageNotFound, PREFIX+'/<path:path>')
+
 
 
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+    print (app.__author__)
+    app.run(port=5000, debug=True if ENV=="QA" else False)
